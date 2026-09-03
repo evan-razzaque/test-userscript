d91a63220a2b5c2f1c404810ad0ad7c3e7bdd7df
evan-razzaque <evan.razzaque@gmail.com>
2026-09-03 16:59:57 -0400

    Remove download and update urls

--- main.meta.js
+++ main.meta.js
@@ -2,8 +2,6 @@
 // @name         Test userscript
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @downloadURL  https://raw.githubusercontent.com/evan-razzaque/test-userscript/refs/heads/main/main.user.js
-// @updateURL    https://raw.githubusercontent.com/evan-razzaque/test-userscript/refs/heads/main/main.meta.js
 // @description  try to take over the world!
 // @author       You
 // @match        https://*/*

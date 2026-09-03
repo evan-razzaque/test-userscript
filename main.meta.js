1463693a030f7c4143fa6e3e882d691513060fd7
evan-razzaque <evan.razzaque@gmail.com>
2026-09-03 17:33:05 -0400

    Test update with hook

--- main.meta.js
+++ main.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Test userscript
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// ==/UserScript==
+// ==UserScript==
--- main.user.js
+++ main.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Test userscript
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// ==/UserScript==
+// ==UserScript==
 
 (function() {
     'use strict';
@@ -16,4 +16,5 @@
 	console.log("test github userscript")
 	console.log("more testing")
 	console.log("even more testing")
+	console.log("can you believe it? More testing!")
 })();

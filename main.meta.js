1e58185eeaea9e5816c002fe223dfe74693c7f58
evan-razzaque <evan.razzaque@gmail.com>
2026-09-03 16:45:44 -0400

    Add userscript file

---
+++ main.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2026-09-02
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
---
+++ main.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2026-09-02
+// @downloadURL  https://raw.githubusercontent.com/evan-razzaque/test-userscript/refs/heads/main/main.user.js
+// @updateURL    https://raw.githubusercontent.com/evan-razzaque/test-userscript/refs/heads/main/main.meta.js
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+	console.log("test github userscript")
+})();
